a49d1b7a11fe5d40b3d522b31810799a11d061fb
Lisandra Tkaczuk <124847222+Lisatka@users.noreply.github.com>
2024-01-25 17:23:20 -0300

    Add files via upload

---
+++ cozinha.java/controller/Interface.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class Interface {package controller;
+
+    public class InterfaceControler {
+        public static String receitasDisponiveis = "";
+        public static String receitasIndisponiveis = "";
+    }
+    
+}
